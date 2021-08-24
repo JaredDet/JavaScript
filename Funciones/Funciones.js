e7c7ea65e01098733f0fe39cdff5cb115c56095a
Javier Marquez <jamarquez145@gmail.com>
2021-08-24 04:58:23 -0400

    Agregado funciones v0.1

---
+++ Funciones/Funciones.js
@@ -0,0 +1,22 @@
+//Funciones
+
+/*
+En JavaScript, un objeto es definido como un JSON,
+es decir, atributo : valor;
+ */
+const persona = {
+    nombre: "Javier",
+    edad: 21,
+}
+
+/*En JavaScript, no hace falta tener que definir el tipo de dato
+que requiere como argumento una función. No obstante, sí es posible
+definir un valor por defecto si no se entrega nada.
+ */
+
+function saludar(nombre = "", edad = -1){
+    console.log(`Hola, me llamo ${nombre} y tengo ${edad} años.`);
+}
+
+saludar();
+saludar(23, "Nombre");
